0de2c7017b7e260c41eaa06370f74c0330cbaeb4
root <aljohns2@andrew.cmu.edu>
2025-09-05 02:38:18 +0000

    Reduced complexity count of getMessagesData

--- src/messaging/data.js
+++ src/messaging/data.js
@@ -45,76 +45,89 @@ module.exports = function (Messaging) {
 		await db.setObject(`message:${mid}`, data);
 	};
 
+	console.log('Andrew Johnson');
 	Messaging.getMessagesData = async (mids, uid, roomId, isNew) => {
-		let messages = await Messaging.getMessagesFields(mids, []);
-		messages = messages
-			.map((msg, idx) => {
-				if (msg) {
-					msg.messageId = utils.isNumber(mids[idx]) ? parseInt(mids[idx], 10) : mids[idx];
-					msg.ip = undefined;
-					msg.isOwner = msg.fromuid === parseInt(uid, 10);
-				}
-				return msg;
-			})
-			.filter(Boolean);
-		messages = await user.blocks.filter(uid, 'fromuid', messages);
-		const users = await user.getUsersFields(
-			messages.map(msg => msg && msg.fromuid),
-			['uid', 'username', 'userslug', 'picture', 'status', 'banned']
-		);
-
-		messages.forEach((message, index) => {
-			message.fromUser = users[index];
-			message.fromUser.banned = !!message.fromUser.banned;
-			message.fromUser.deleted = message.fromuid !== message.fromUser.uid && message.fromUser.uid === 0;
-
-			const self = message.fromuid === parseInt(uid, 10);
-			message.self = self ? 1 : 0;
-
-			message.newSet = false;
-			message.roomId = String(message.roomId || roomId);
-		});
-
-		await parseMessages(messages, uid, roomId, isNew);
-
-		if (messages.length > 1) {
-			// Add a spacer in between messages with time gaps between them
-			messages = messages.map((message, index) => {
-				// Compare timestamps with the previous message, and check if a spacer needs to be added
+		const parsedUid = parseInt(uid, 10);
+	
+		const normalizeMessages = (mids, msgs, parsedUid) =>
+			msgs
+				.map((msg, idx) => {
+					if (msg) {
+						msg.messageId = utils.isNumber(mids[idx]) ? parseInt(mids[idx], 10) : mids[idx];
+						msg.ip = undefined;
+						msg.isOwner = msg.fromuid === parsedUid;
+					}
+					return msg;
+				})
+				.filter(Boolean);
+	
+		const attachFromUsers = (messages, users, parsedUid, roomId) => {
+			messages.forEach((message, index) => {
+				message.fromUser = users[index];
+				message.fromUser.banned = !!message.fromUser.banned;
+				message.fromUser.deleted = message.fromuid !== message.fromUser.uid && message.fromUser.uid === 0;
+				const self = message.fromuid === parsedUid;
+				message.self = self ? 1 : 0;
+				message.newSet = false;
+				message.roomId = String(message.roomId || roomId);
+			});
+			return messages;
+		};
+	
+		const setNewSetForMultiple = messages =>
+			messages.map((message, index) => {
 				if (index > 0 && message.timestamp > messages[index - 1].timestamp + Messaging.newMessageCutoff) {
-					// If it's been 5 minutes, this is a new set of messages
 					message.newSet = true;
 				} else if (index > 0 && message.fromuid !== messages[index - 1].fromuid) {
-					// If the previous message was from the other person, this is also a new set
 					message.newSet = true;
 				} else if (index > 0 && messages[index - 1].system) {
 					message.newSet = true;
 				} else if (index === 0 || message.toMid) {
 					message.newSet = true;
 				}
-
 				return message;
 			});
-		} else if (messages.length === 1) {
-			// For single messages, we don't know the context, so look up the previous message and compare
+	
+		const setNewSetForSingle = async (messages, roomId) => {
 			const key = `chat:room:${roomId}:mids`;
 			const index = await db.sortedSetRank(key, messages[0].messageId);
 			if (index > 0) {
 				const mid = await db.getSortedSetRange(key, index - 1, index - 1);
 				const fields = await Messaging.getMessageFields(mid, ['fromuid', 'timestamp']);
-				if ((messages[0].timestamp > fields.timestamp + Messaging.newMessageCutoff) ||
-					(messages[0].fromuid !== fields.fromuid) ||
-					messages[0].system || messages[0].toMid) {
-					// If it's been 5 minutes, this is a new set of messages
+				if (
+					messages[0].timestamp > fields.timestamp + Messaging.newMessageCutoff ||
+					messages[0].fromuid !== fields.fromuid ||
+					messages[0].system ||
+					messages[0].toMid
+				) {
 					messages[0].newSet = true;
 				}
 			} else {
 				messages[0].newSet = true;
 			}
+			return messages;
+		};
+	
+		let messages = await Messaging.getMessagesFields(mids, []);
+		messages = normalizeMessages(mids, messages, parsedUid);
+		messages = await user.blocks.filter(uid, 'fromuid', messages);
+	
+		const users = await user.getUsersFields(
+			messages.map(msg => msg && msg.fromuid),
+			['uid', 'username', 'userslug', 'picture', 'status', 'banned']
+		);
+	
+		messages = attachFromUsers(messages, users, parsedUid, roomId);
+		await parseMessages(messages, uid, roomId, isNew);
+	
+		if (messages.length > 1) {
+			messages = setNewSetForMultiple(messages);
+		} else if (messages.length === 1) {
+			messages = await setNewSetForSingle(messages, roomId);
 		}
-
+	
 		await addParentMessages(messages, uid, roomId);
-
+	
 		const data = await plugins.hooks.fire('filter:messaging.getMessages', {
 			messages: messages,
 			uid: uid,
@@ -122,10 +135,11 @@ module.exports = function (Messaging) {
 			isNew: isNew,
 			mids: mids,
 		});
-
+	
 		return data && data.messages;
 	};
-
+	
+	
 	async function addParentMessages(messages, uid, roomId) {
 		let parentMids = messages.map(msg => (msg && msg.hasOwnProperty('toMid') ? parseInt(msg.toMid, 10) : null)).filter(Boolean);
 
